addeeb1ac7c79c8fdec4d64a7fe7f6d5a858621c
vincent desmouceaux <vincentdesmont@mbp-de-vincent.lan>
2022-11-08 14:22:08 +0100

    update4

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import logo from "./img/Deliveroo-Logo.png";
 
 function App() {
-  const [counter, setCounter] = useState(0);
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
@@ -24,10 +23,79 @@ function App() {
       <div>
         <header className="Header">
           <div className="topBar">
-            <img className="logo" src={logo} alt="delivroo logo" />
+            <div className="topbarcenter">
+              <img className="logo" src={logo} alt="delivroo logo" />
+            </div>
           </div>
-          <div className="RestaurantInfos"></div>
+          <div className="RestaurantInfos">
+            <div className="RestaurantInfos--center">
+              <div className="restaurantInfostext">
+                <h1> {data.restaurant.name}</h1>
+                <p>{data.restaurant.description}</p>
+              </div>
+              <img
+                className="Restaurantimg"
+                src="https://f.roocdn.com/images/menus/17697/header-image.jpg"
+                alt="main photo"
+              />
+            </div>
+          </div>
         </header>
+        <div className="Content">
+          <div className="Content--center">
+            <div className="menu">
+              {data.categories.map((elem, index) => {
+                return (
+                  <div key={index}>
+                    <div className="menuitems">
+                      <h2>{elem.name}</h2>
+
+                      <div className="menuitemitem">
+                        {elem.meals.map((meal, index) => {
+                          return (
+                            <div className="menuitem" key={meal.id}>
+                              <div className="menuitemsCard">
+                                <div className="menuitemstext">
+                                  <h3> {meal.title}</h3>
+                                  <p>{meal.description}</p>
+                                  <div className="menuInfo">
+                                    <span className="menuitemprice">
+                                      {meal.price}
+                                    </span>
+                                    {meal.popular && (
+                                      <span className="menuitempop">
+                                        populaire
+                                      </span>
+                                    )}
+                                  </div>
+                                </div>
+                                <div className="menuitempic">
+                                  <img
+                                    className="menuImg"
+                                    src={meal.picture}
+                                    alt="pics"
+                                  />
+                                </div>
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="Cart">
+              <div className="Cart--Card">
+                <button className="Cart--Validate Cart--disabled">
+                  Valider mon panier
+                </button>
+                <div className="Cart-empty">Votre panier est vide</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </body>
   );
